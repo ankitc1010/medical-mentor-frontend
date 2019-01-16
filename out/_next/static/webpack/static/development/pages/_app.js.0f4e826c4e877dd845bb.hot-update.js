d5b9142921e5dee70873b6d560ed5dde6ef097d1
Ankit Chauhan <ankit.chauhan@unifize.com>
2019-01-16 19:09:59 +0530

    Changed now env variable

---
+++ out/_next/static/webpack/static/development/pages/_app.js.0f4e826c4e877dd845bb.hot-update.js
@@ -0,0 +1,23 @@
+webpackHotUpdate("static/development/pages/_app.js",{
+
+/***/ "./config.js":
+/*!*******************!*\
+  !*** ./config.js ***!
+  \*******************/
+/*! exports provided: endpoint, prodEndpoint, perPage */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "endpoint", function() { return endpoint; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "prodEndpoint", function() { return prodEndpoint; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "perPage", function() { return perPage; });
+// This is client side config only - don't put anything in here that shouldn't be public!
+var endpoint = "http://localhost:4444";
+var prodEndpoint = "https://out-2zd5byg98.now.sh";
+var perPage = 4;
+
+/***/ })
+
+})
+//# sourceMappingURL=_app.js.0f4e826c4e877dd845bb.hot-update.js.map
---
+++ out/_next/static/webpack/static/development/pages/_app.js.bc3b0a73d65a65241d9e.hot-update.js
@@ -0,0 +1,23 @@
+webpackHotUpdate("static/development/pages/_app.js",{
+
+/***/ "./config.js":
+/*!*******************!*\
+  !*** ./config.js ***!
+  \*******************/
+/*! exports provided: endpoint, prodEndpoint, perPage */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "endpoint", function() { return endpoint; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "prodEndpoint", function() { return prodEndpoint; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "perPage", function() { return perPage; });
+// This is client side config only - don't put anything in here that shouldn't be public!
+var endpoint = "http://localhost:4444";
+var prodEndpoint = "https://hidden-waters-40385.herokuapp.com";
+var perPage = 4;
+
+/***/ })
+
+})
+//# sourceMappingURL=_app.js.bc3b0a73d65a65241d9e.hot-update.js.map
