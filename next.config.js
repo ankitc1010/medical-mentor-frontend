a3a7f5ffbc291e31e4a2168eaf7e5d3e1b7b69af
Ankit Chauhan <ankit.chauhan@unifize.com>
2019-01-22 08:42:25 +0530

    Added package-lock ad yarn.lock

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const withImages = require("next-images");
 const withCSS = require("@zeit/next-css");
-const withBabelMinify = require("next-babel-minify")();
 const withPlugins = require("next-compose-plugins");
-module.exports = withBabelMinify(withCSS(withImages()));
+module.exports = withCSS(withImages());
