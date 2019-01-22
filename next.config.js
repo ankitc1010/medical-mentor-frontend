dbf6c3de1b8ef4a0f3244b32fe28af927ff1d80a
Ankit Chauhan <ankit.chauhan@unifize.com>
2019-01-22 08:26:42 +0530

    Edite next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 const withImages = require("next-images");
 const withCSS = require("@zeit/next-css");
-const withBabelMinify = require("next-babel-minify")({
-  comments: false,
-  compress: {
-    // remove warnings
-    warnings: process.env.NODE_ENV === "development" ? false : true,
-
-    // Drop console statements
-    drop_console: process.env.NODE_ENV === "development" ? false : true
-  }
-});
+const withBabelMinify = require("next-babel-minify")();
 const withPlugins = require("next-compose-plugins");
 module.exports = withBabelMinify(withCSS(withImages()));
